f130d66eb53131d431b245c5e442c6862f018156
TheRadioStar <gunner10081@gmail.com>
2026-02-27 07:31:27 -0500

    first update...

---
+++ PlayTriviaGame.py
@@ -0,0 +1,6 @@
+def main():
+    questionLst = [["What is 2+2?"], ["What does H2O mean?"], ["Who was the first president?"], ["Who wrote "
+                                                                                                 "Frankenstein?"],
+                   [], [],
+                   [], []]
+    answerKey = []
